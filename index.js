4048efc95b5c75bb55b402185eb108822f0d8b3b
mkay <mkay@gmail.com>
2025-03-16 23:33:19 +0300

    we are on

--- index.js
+++ index.js
@@ -1,47 +1,3 @@
-
-function studentRollCall(student) {
-  return student + " the skier";
-}
-
-function studentRollCall(student) {
-  return student + " the skier";
-}
-
-const skiSchool = ["aki", "guadalupe", "lei", "aalam"];
-const rollCall = skiSchool.map(studentRollCall);
-console.log(skiSchool)
-
-const rollCall2 = [];
-
-for (const student of skiSchool) {
-rollCall2.push(student + " the skier");
-}
-console.log(skiSchool)
-
-
-const rollCall3 = skiSchool.map(function (student) {
-  return student + " the skier";
-});
-console.log(skiSchool)
-
-
-
-const robots = [
-  { name: "Johnny 5", modes: 5, isActivated: false },
-  { name: "C3PO", modes: 3, isActivated: false },
-  { name: "Sonny", modes: 2.5, isActivated: false },
-  { name: "Baymax", modes: 1.5, isActivated: false },
-];
-
-const activatedRobots = robots.map((robot) => {
-  return Object.assign({}, robot, {
-    modes: robot.modes * 2,
-    isActivated: true,
-  });
-});
-
-console.log(activatedRobots);
-
 const tutorials = [
   "what does the this keyword mean?",
   "What is the Constructor OO pattern?",
@@ -56,12 +12,12 @@ const tutorials = [
 ];
 
 function titleCased() {
-  return tutorials.map((title) => {
-    return title
+  return tutorials.map((tutorial) =>
+    tutorial
       .split(" ") 
-      .map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase()) 
-      .join(" "); 
-  });
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1)) 
+      .join(" ") 
+  );
 }
 
 console.log(titleCased());
